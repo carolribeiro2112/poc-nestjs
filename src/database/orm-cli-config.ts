1bdd9f32080b1e5428df0fd55a5577c1a91e8bd1
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2024-01-24 17:36:06 -0300

    Feat: creating Tags Table with typeorm

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1706108627456 } from 'src/migrations/1706108627456-CreateCoursesTable';
+import { CreateTagsTable1706127922642 } from 'src/migrations/1706127922642-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1706108627456],
+  migrations: [CreateCoursesTable1706108627456, CreateTagsTable1706127922642],
 });
